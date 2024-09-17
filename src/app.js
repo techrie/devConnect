f2b5062cd569d58f4679ced17acf7ae93e9ce876
p0oo <cute.n.charming@gmail.com>
2024-09-17 11:02:34 -0700

    exploring api's

--- src/app.js
+++ src/app.js
@@ -2,6 +2,25 @@ const express = require("express");
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+//Order of routes is important
+
+app.get("/user", (req, res) => {
+  res.send({ firstname: "USer", lastname: "Da" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("Data saved successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("User deleted");
+});
+
+//this will match all HTTP method API calls to /hello
+app.use("/hello", (req, res) => {
+  res.send("Hello hello hello");
+});
+
+app.listen(7777, () => {
+  console.log("Server is listening on port 7777");
 });
